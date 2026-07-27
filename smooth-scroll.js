258cde1f499baeaac1e065311abb6fa524e58d7a
Adrian Ramos Marquez <adrianteeeen@gmail.com>
2026-07-27 20:37:28 +0200

    Add files via upload

--- smooth-scroll.js
+++ smooth-scroll.js
@@ -4,7 +4,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   if (typeof Lenis !== 'undefined') {
     const lenis = new Lenis({
-      lerp: 0.2,           // Un valor más alto hace que responda al instante (puedes subirlo a 0.25 si lo quieres más inmediato)
+      lerp: 0.8,           // Un valor más alto hace que responda al instante (puedes subirlo a 0.25 si lo quieres más inmediato)
       wheelMultiplier: 1,  
       smoothWheel: true,
       sync: true,          // Fuerza la sincronización inmediata con el hilo principal del navegador (elimina la pausa)
